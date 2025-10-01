da25b85d97e5eec1f2df03f24f58508fc5da540b
aditya1234-design <144038351+aditya1234-design@users.noreply.github.com>
2025-10-01 22:02:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function downloadFile(filePath) {
-    const a = document.createElement('a');
-    a.href = filePath;
-    a.download = '';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-}
